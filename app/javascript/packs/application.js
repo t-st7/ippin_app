d73a7ba9bb18014bf4e0426c34bed9255649e061
t-st7 <76399553+t-st7@users.noreply.github.com>
2021-02-22 20:28:56 +0900

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,7 @@ require('jquery')
 require("./preview")
 // require('jquery_nested_form')
 require("@nathanvda/cocoon")
-// require('./ingredient')
+require('./ingredient')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
