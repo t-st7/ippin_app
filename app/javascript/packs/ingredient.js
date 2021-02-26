7041ba69b5434a6bdcdbb44d680d09d8aea4c9f6
t-st7 <76399553+t-st7@users.noreply.github.com>
2021-02-26 21:10:46 +0900

    変更

--- app/javascript/packs/ingredient.js
+++ app/javascript/packs/ingredient.js
@@ -4,9 +4,29 @@ if (location.pathname.match("/")){
     inputElement.addEventListener("keyup", () =>{
       const keyword = document.getElementById("search-form").value;
       const XHR = new XMLHttpRequest();
-      XHR.open("GET", `search/@q_search`,true)
+      XHR.open("GET", `search/?keyword=${keyword}`,true)
       XHR.responseType = "json";
       XHR.send();
+      XHR.onload = () => {
+ 
+        const searchResult = document.getElementById("search-result");
+        searchResult.innerHTML = "";
+        if (XHR.response) {
+          const tagName = XHR.response.keyword;
+          tagName.forEach((post) => {
+            const childElement = document.createElement("div");
+            childElement.setAttribute("class", "child");
+            childElement.setAttribute("id", post.id);
+            childElement.innerHTML = post.title;
+            searchResult.appendChild(childElement);
+            const clickElement = document.getElementById(post.id);
+            clickElement.addEventListener("click", () => {
+              document.getElementById("search-form").value = clickElement.textContent;
+              clickElement.remove();
+            });
+          });
+        };
+      };
     });
   });
 };
